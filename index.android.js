f7e6e4b0cb273b3feb9592d01bb7454e4042fadc
sunp <664006332@qq.com>
2017-06-15 18:07:19 +0800

    +组件生命周期 +state使用示例

--- index.android.js
+++ index.android.js
@@ -3,6 +3,7 @@
  */
 
 import {AppRegistry} from "react-native";
-import App from './src/Case/calc/Main';
+// import App from './src/Case/calc/Main';
+import App from './src/Demo/State/BtnClickDisappear';
 
 AppRegistry.registerComponent('RNLearning', () => App);
